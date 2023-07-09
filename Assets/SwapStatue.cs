f5472496cae25432822bcc558c0ff2afa703f1f6
Luke Tweedy <luke.tweedy@gmail.com>
2023-07-09 16:54:41 +0100

    Pushing statue swapper.

--- Assets/SwapStatue.cs
+++ Assets/SwapStatue.cs
@@ -9,10 +9,18 @@ public class SwapStatue : MonoBehaviour
 
     public MeshFilter target;
 
+    public Vector3 Alt1Position;
+    public Vector3 Alt2Position;
+
+    public Vector3 Alt1Rotation;
+    public Vector3 Alt2Rotation;
+
     // Start is called before the first frame update
     void OnEnable()
     {
-        int selection = Random.RandomRange(0, 2);
+        int selection = Random.RandomRange(0, 3);
+
+        print("Selection is " + selection.ToString());
 
         if (selection == 0) return;
 
@@ -23,12 +31,18 @@ public class SwapStatue : MonoBehaviour
             target.sharedMesh = Alt1;
             mc.sharedMesh = Alt1;
             mc.inflateMesh = true;
+
+            target.transform.localPosition = Alt1Position;
+            target.transform.localEulerAngles = Alt1Rotation;
         }
         else if(selection == 2)
         {
             target.sharedMesh = Alt2;
             mc.sharedMesh = Alt2;
             mc.inflateMesh = true;
+
+            target.transform.localPosition    = Alt2Position;
+            target.transform.localEulerAngles = Alt2Rotation;
         }
 
     }
